dd7b60748a73ffdf673138599a0ddc4ce90d3f33
Nathan Reynolds <email@nreynolds.co.uk>
2014-11-05 20:47:28 +0000

    Show started_at on admin

--- src/wiretap/admin.py
+++ src/wiretap/admin.py
@@ -9,7 +9,7 @@ class TapAdmin(admin.ModelAdmin):
     list_display = ('path_regex',)
 
 class MessageAdmin(ReadOnlyModelAdmin):
-    list_display = ('request', 'response', 'remote_addr', 'duration',)
+    list_display = ('request', 'response', 'remote_addr', 'started_at', 'duration',)
     list_filter = ('started_at', 'req_method', 'res_status_code',)
     search_fields = ('remote_addr', 'req_path',)
 
